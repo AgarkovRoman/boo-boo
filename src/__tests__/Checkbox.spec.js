07341b5898d55f1af7d29530deee49bdcfb70a3d
Roman Agarkov <agarkovromachka@mail.ru>
2020-12-29 23:45:54 +0300

    refactor checkbox tests

--- src/__tests__/Checkbox.spec.js
+++ src/__tests__/Checkbox.spec.js
@@ -17,21 +17,21 @@ jest.mock('../firebase', () => ({
 describe('<Checkbox/>', () => {
   describe('Success', () => {
     it('render the task checkbox', () => {
-      const { getByTestId } = render(<Checkbox id={'1'} taskDesc={'Finish this app'} />)
+      const { getByTestId } = render(<Checkbox id="1" taskDesc="Finish this app" />)
       expect(getByTestId('checkbox-action')).toBeTruthy()
     })
     it('render the task checkbox and accepts a onClick', () => {
-      const { getByTestId } = render(<Checkbox id={'1'} taskDesc={'Finish this app'} />)
+      const { getByTestId } = render(<Checkbox id="1" taskDesc="Finish this app" />)
       expect(getByTestId('checkbox-action')).toBeTruthy()
       fireEvent.click(getByTestId('checkbox-action'))
     })
     it('render the task checkbox and accepts a Enter onKeyDown', () => {
-      const { getByTestId } = render(<Checkbox id={'1'} taskDesc={'Finish this app'} />)
+      const { getByTestId } = render(<Checkbox id="1" taskDesc="Finish this app" />)
       expect(getByTestId('checkbox-action')).toBeTruthy()
       fireEvent.keyDown(getByTestId('checkbox-action'), { key: 'Enter', code: 'Enter' })
     })
     it('render the task checkbox and accepts a any onKeyDown', () => {
-      const { getByTestId } = render(<Checkbox id={'1'} taskDesc={'Finish this app'} />)
+      const { getByTestId } = render(<Checkbox id="1" taskDesc="Finish this app" />)
       expect(getByTestId('checkbox-action')).toBeTruthy()
       fireEvent.keyDown(getByTestId('checkbox-action'), { key: 'a', code: 'KeyA' })
     })
