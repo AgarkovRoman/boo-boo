e54f05c0ad1dca0a8588e260e4502b685d7c9624
Roman Agarkov <agarkovromachka@mail.ru>
2020-12-30 22:26:42 +0300

    refactor

--- src/__tests__/Checkbox.test.js
+++ src/__tests__/Checkbox.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Checkbox } from '../components/UI/Checkbox/Checkbox'
 
 jest.mock('../firebase', () => ({
@@ -23,7 +24,7 @@ describe('<Checkbox/>', () => {
     it('render the task checkbox and accepts a onClick', () => {
       const { getByTestId } = render(<Checkbox id="1" taskDesc="Finish this app" />)
       expect(getByTestId('checkbox-action')).toBeTruthy()
-      fireEvent.click(getByTestId('checkbox-action'))
+      userEvent.click(getByTestId('checkbox-action'))
     })
     it('render the task checkbox and accepts a Enter onKeyDown', () => {
       const { getByTestId } = render(<Checkbox id="1" taskDesc="Finish this app" />)
