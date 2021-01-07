29d9332651b6adfb0ee884d494438979a4dae517
Roman Agarkov <agarkovromachka@mail.ru>
2021-01-07 15:39:07 +0300

    refactor signin

--- src/components/pages/SignIn/SignIn.tsx
+++ src/components/pages/SignIn/SignIn.tsx
@@ -1,61 +1,75 @@
-import React, {useContext, useState} from 'react'
+import React, { useContext, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { NavLink, useHistory } from 'react-router-dom'
 import classes from './SignIn.module.scss'
-import {useForm} from 'react-hook-form';
-import {NavLink, useHistory} from "react-router-dom";
 import * as ROUTER from '../../../constants/routes'
-import {FirebaseContext} from "../../../context/firebase";
+import { FirebaseContext } from '../../../context/firebase'
 
 type FormData = {
-    Email: string
-    Password: string
+  Email: string
+  Password: string
 }
 
 export const SignIn: React.FC = () => {
-    const history = useHistory()
-    const {firebase}: any = useContext(FirebaseContext)
-    const {register, handleSubmit, errors} = useForm<FormData>();
-    const [error, setError] = useState()
+  const history = useHistory()
+  const { firebase }: any = useContext(FirebaseContext)
+  const { register, handleSubmit, errors } = useForm<FormData>()
+  const [error, setError] = useState()
 
-    const onSubmit = (data: FormData) => {
-        console.log("data", data);
-        firebase
-            .auth()
-            .signInWithEmailAndPassword(data.Email, data.Password)
-            .then(() => {
-                history.push(ROUTER.APP)
-            })
-            .catch((error: any) => {
-                setError(error.message)
-            })
-    };
+  const onSubmit = (data: FormData) => {
+    console.log('data', data)
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(data.Email, data.Password)
+      .then(() => {
+        history.push(ROUTER.APP)
+      })
+      .catch((error: any) => {
+        setError(error.message)
+      })
+  }
 
-    console.log(errors);
+  console.log(errors)
 
-    return (
-        <div className={classes.wrapper}>
-            <div className={classes.formBox}>
-                <h3 className={classes.title}>Вход</h3>
-                {error}
-                <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
-                    <label className={classes.label}>Email
-                        <input type="text" placeholder="Email" name="Email"
-                               ref={register({required: true, pattern: /^\S+@\S+$/i})}/>
-                        {/*{errors.Email && <p>Обязательное поле</p>}*/}
-                    </label>
-                    <label className={classes.label}>Password
-                        <input type="password" placeholder="Password" name="Password"
-                               ref={register({required: true, minLength: 6})}/>
-                        {/*{errors.Password && <p>Обязательное поле</p>}*/}
-                    </label>
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.formBox}>
+        <h3 className={classes.title}>Вход</h3>
+        {error}
+        <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+          <label className={classes.label}>
+            Email
+            <input
+              type="text"
+              placeholder="Email"
+              name="Email"
+              ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+            />
+            {/* {errors.Email && <p>Обязательное поле</p>} */}
+          </label>
 
-                    <button
-                        className={classes.button}
-                        type="submit"
-                    >Войти
-                    </button>
-                </form>
-                <p>Ещё нет аккаунта? <NavLink className={classes.signupLink} to={ROUTER.SIGN_UP}>Sign up now</NavLink></p>
-            </div>
-        </div>
-    )
+          <label className={classes.label}>
+            Password
+            <input
+              type="password"
+              placeholder="Password"
+              name="Password"
+              ref={register({ required: true, minLength: 6 })}
+            />
+            {/* {errors.Password && <p>Обязательное поле</p>} */}
+          </label>
+          <button className={classes.button} type="submit">
+            Войти
+          </button>
+        </form>
+
+        <p>
+          Ещё нет аккаунта?
+          <NavLink className={classes.signupLink} to={ROUTER.SIGN_UP}>
+            Sign up now
+          </NavLink>
+        </p>
+      </div>
+    </div>
+  )
 }
