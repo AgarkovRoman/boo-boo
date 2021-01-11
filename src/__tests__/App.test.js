28e2347d7a715849262b160492f408bd35b2df7e
Roman Agarkov <agarkovromachka@mail.ru>
2021-01-11 22:48:28 +0300

    refactor tests

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { App } from '../App'
+
+describe('<App/>', () => {
+  it('render the application', () => {
+    const { getByTestId } = render(<App />)
+    expect(getByTestId('application')).toBeTruthy()
+    expect(getByTestId('application').classList.contains('darkmode')).toBeFalsy()
+  })
+  it('render the application using dark mode', () => {
+    const { getByTestId } = render(<App darkModeDefault />)
+    expect(getByTestId('application')).toBeTruthy()
+    expect(getByTestId('application').classList.contains('darkmode')).toBeTruthy()
+  })
+})
