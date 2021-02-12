66f7ff58717eab35cc133be6a2fb29e6bc8925d4
Roman Agarkov <agarkovromachka@mail.ru>
2021-02-12 11:22:20 +0300

    add setAllProjectThunkCreator

--- src/redux/projects-reducer.js
+++ src/redux/projects-reducer.js
@@ -1,19 +1,28 @@
 import { INBOX } from '../constants/defaultProjects'
+import { projectsAPI } from '../api/api'
 
-const SET_PROJECT = 'SET_PROJECT'
+const SET_ACTIVE_PROJECT = 'SET_ACTIVE_PROJECT'
+const SET_ALL_PROJECTS = 'SET_ALL_PROJECTS'
 
 const initialState = {
   activeProject: INBOX,
+  allProjects: [],
 }
 
 export const projectsReducer = (state = initialState, action) => {
   const { type, payload } = action
 
   switch (type) {
-    case SET_PROJECT: {
+    case SET_ACTIVE_PROJECT: {
       return {
         ...state,
         activeProject: payload,
+      }
+    }
+    case SET_ALL_PROJECTS: {
+      return {
+        ...state,
+        allProjects: payload,
       }
     }
     default:
@@ -21,4 +30,15 @@ export const projectsReducer = (state = initialState, action) => {
   }
 }
 
-export const setProject = (project) => ({ type: SET_PROJECT, payload: project })
+export const setProject = (project) => ({ type: SET_ACTIVE_PROJECT, payload: project })
+export const setAllProjects = (projects) => ({ type: SET_ALL_PROJECTS, payload: projects })
+
+export const setAllProjectThunkCreator = (userId) => async (dispatch) => {
+  await projectsAPI.getAllProjectsById(userId).then((snapshot) => {
+    const allProjects = snapshot.docs.map((project) => ({
+      ...project.data(),
+      docId: project.id,
+    }))
+    dispatch(setAllProjects(allProjects))
+  })
+}
