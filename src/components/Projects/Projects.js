fba252f2a7fecf52690424bf9d296896b74e09cb
Roman Agarkov <agarkovromachka@mail.ru>
2021-02-13 09:45:15 +0300

    refactor

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,19 +1,17 @@
 import React, { useCallback, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import classes from './Projects.module.scss'
-import { useProjectsValue, useSelectedProjectsValue } from '../../context'
 import { IndividualProject } from '../IndividualProject/IndividualProject'
 import { setProject } from '../../redux/projects-reducer'
 
 export const Projects = ({ activeNull = true }) => {
   const [active, setActive] = useState(activeNull)
-  const { setSelectedProject } = useSelectedProjectsValue()
-  const { projects } = useProjectsValue()
-
   const dispatch = useDispatch()
+
+  const projects = useSelector((state) => state.projects.allProjects)
+
   const selectProject = useCallback((project) => dispatch(setProject(project)), [dispatch])
 
-  // console.log('projects', projects)
   return (
     projects &&
     projects.map((project) => (
@@ -31,13 +29,11 @@ export const Projects = ({ activeNull = true }) => {
           aria-label={`Select ${project.name} as the task project`}
           onClick={() => {
             setActive(project.projectId)
-            setSelectedProject(project.projectId)
             selectProject(project.projectId)
           }}
           onKeyDown={(e) => {
             if (e.key === 'Enter') {
               setActive(project.projectId)
-              setSelectedProject(project.projectId)
               selectProject(project.projectId)
             }
           }}
