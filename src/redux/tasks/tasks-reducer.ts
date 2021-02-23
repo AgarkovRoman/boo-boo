fdb3cd75220ee02e0b2e246d808455f1fbd9223e
Roman Agarkov <agarkovromachka@mail.ru>
2021-02-23 23:48:18 +0300

    rewrite on ts

---
+++ src/redux/tasks/tasks-reducer.ts
@@ -0,0 +1,100 @@
+import { ThunkAction } from 'redux-thunk'
+import { Action } from '@reduxjs/toolkit'
+import { tasksAPI } from '../../api/api'
+import {
+  addTaskActionCreatorI,
+  archivedTaskActionCreatorI,
+  setAllTasksActionCreatorI,
+  TaskI,
+  TasksActionCreatorType,
+  TasksI,
+  TasksStateI,
+} from './tasks-types'
+
+const initialState: TasksI = {
+  allTasks: [],
+}
+
+export const SET_TASKS = 'SET_TASKS'
+export const ADD_TASK = 'ADD_TASK'
+export const ARCHIVED_TASK = 'ARCHIVED_TASK'
+
+const archivedTaskHandler = (tasks: Array<TaskI>, taskId: Array<TaskI> | TaskI | string) =>
+  tasks.map((task) => {
+    if (task.id !== taskId) {
+      return { ...task }
+    }
+    return {
+      ...task,
+      archived: true,
+    }
+  })
+
+export const tasksReducer = (state = initialState, action: TasksActionCreatorType) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case SET_TASKS: {
+      return { ...state, allTasks: payload } as TasksI
+    }
+    case ADD_TASK: {
+      return { ...state, allTasks: [...state.allTasks, payload] } as TasksI
+    }
+    case ARCHIVED_TASK: {
+      return { ...state, allTasks: archivedTaskHandler(state.allTasks, payload) } as TasksI
+    }
+    default:
+      return state
+  }
+}
+
+export const setAllTasks = (tasks: Array<TaskI>): setAllTasksActionCreatorI => ({
+  type: SET_TASKS,
+  payload: tasks,
+})
+
+export const addTask = (task: TaskI): addTaskActionCreatorI => ({ type: ADD_TASK, payload: task })
+
+export const archivedTask = (taskId: string): archivedTaskActionCreatorI => ({
+  type: ARCHIVED_TASK,
+  payload: taskId,
+})
+
+export const getAllTasksTC = (
+  userId: string
+): ThunkAction<void, TasksStateI, unknown, Action> => async (dispatch) => {
+  await tasksAPI.getAllTasksById(userId).then((snapshot) => {
+    const allTasks = snapshot.docs.map((task) => {
+      const data = task.data()
+      return {
+        task: data.task,
+        createDate: data.createDate,
+        date: data.date,
+        archived: data.archived,
+        userId: data.userId,
+        projectId: data.projectId,
+        id: task.id,
+        docId: task.id,
+      }
+    })
+
+    const sortTasks = allTasks.sort((a, b) => a.createDate - b.createDate)
+    dispatch(setAllTasks(sortTasks))
+  })
+}
+
+export const addTaskTC = (
+  task: TaskI
+): ThunkAction<void, TasksStateI, unknown, Action<string>> => async (dispatch) => {
+  dispatch(addTask(task))
+  await tasksAPI.addTask(task).then(() => {
+    dispatch(getAllTasksTC(task.userId))
+  })
+}
+
+export const archiveTaskTC = (
+  taskId: string
+): ThunkAction<void, TasksStateI, unknown, Action> => async (dispatch) => {
+  await tasksAPI.archivedTasksById(taskId)
+  dispatch(archivedTask(taskId))
+}
---
+++ src/redux/tasks/tasks-types.ts
@@ -0,0 +1,40 @@
+import { ADD_TASK, ARCHIVED_TASK, SET_TASKS } from './tasks-reducer'
+
+export interface TaskI {
+  task: string
+  createDate: number
+  date: string
+  archived: boolean
+  userId: string
+  projectId: string
+  id: string
+  docId: string
+}
+
+export interface TasksI {
+  allTasks: Array<TaskI>
+}
+
+export interface TasksStateI {
+  tasks: TasksI
+}
+
+export interface setAllTasksActionCreatorI {
+  type: typeof SET_TASKS
+  payload: Array<TaskI>
+}
+
+export interface addTaskActionCreatorI {
+  type: typeof ADD_TASK
+  payload: TaskI
+}
+
+export interface archivedTaskActionCreatorI {
+  type: typeof ARCHIVED_TASK
+  payload: string
+}
+
+export type TasksActionCreatorType =
+  | setAllTasksActionCreatorI
+  | addTaskActionCreatorI
+  | archivedTaskActionCreatorI
