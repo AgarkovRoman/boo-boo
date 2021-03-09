a3b02d2c49d691a5d6f9d12f31e78d39e0574f87
Roman Agarkov <agarkovromachka@mail.ru>
2021-03-10 00:43:44 +0300

    refactor

--- src/__tests__/utils/renderWithRedux.js
+++ src/__tests__/utils/renderWithRedux.js
@@ -1,9 +1,7 @@
-import { createStore } from 'redux'
 import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import React from 'react'
 import { configureStore } from '@reduxjs/toolkit'
-import { projectsReducer } from '../../redux/projects/projects-reducer'
 import { reducer } from '../../redux/redux-store'
 
 export const renderWithRedux = (
