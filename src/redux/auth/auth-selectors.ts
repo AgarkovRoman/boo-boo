91be74ed4591baa3507d39bda0e4c358f898ffc8
Roman Agarkov <agarkovromachka@mail.ru>
2021-11-13 21:34:40 +0300

    UPDATE Selector

--- src/redux/auth/auth-selectors.ts
+++ src/redux/auth/auth-selectors.ts
@@ -1,4 +1,4 @@
 import { AuthStateI, UserI } from './auth-types'
 
-export const getUserId = (state: AuthStateI): string => state.auth.user.userId
-export const getUser = (state: AuthStateI): UserI => state.auth.user
+// export const getUserId = (state: AuthStateI): string => state.auth.user.userId
+export const getUser = (state: AuthStateI): UserI => state.auth.user || {}
