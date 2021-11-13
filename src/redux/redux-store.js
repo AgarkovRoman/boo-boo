6b2b6bd1f90e7c0fd54e5079cfa22fc270a517c6
Roman Agarkov <agarkovromachka@mail.ru>
2021-11-13 21:44:56 +0300

    UPDATE store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authReducer } from './auth/auth-reducer'
-import { projectsReducer } from './projects/projects-reducer'
+import projectsReducer from './projects/projects-reducer'
 import { tasksReducer } from './tasks/tasks-reducer'
+import { projects2API } from '../api/api'
 
-export const reducer = {
-  auth: authReducer,
-  projects: projectsReducer,
-  tasks: tasksReducer,
-}
-
-export const store = configureStore({ reducer })
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    projects: projectsReducer,
+    tasks: tasksReducer,
+    [projects2API.reducerPath]: projects2API.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(projects2API.middleware),
+})
