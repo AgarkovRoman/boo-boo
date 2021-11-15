52a9f55d23a9419f31d5a48447ac0c5738670dbd
Roman Agarkov <agarkovromachka@mail.ru>
2021-11-15 13:08:21 +0300

    rewrite auth logic

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import axios from 'axios'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 import { CreateTaskI, DeleteTaskI, TaskI } from '../redux/tasks/tasks-types'
 import { CreateProjectI, DeleteProjectI, ProjectI } from '../redux/projects/projects-types'
@@ -8,31 +7,8 @@ import { LogoutResponseI, SignInI, UserIServer } from '../redux/auth/auth-types'
 // const BASE_URL = 'https://boo-boo-server.herokuapp.com/api/'
 const BASE_URL = 'http://localhost:8080/api/'
 
-const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-})
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    if (
-      typeof config.url === 'string' &&
-      (config.url.includes('/auth/login') || config.url.includes('/auth/registration'))
-    ) {
-      return config
-    }
-    const authUser = JSON.parse(localStorage.getItem('authUser') || '')
-    if (config.headers) {
-      config.headers.Authorization = authUser ? `Bearer ${authUser?.accessToken}` : ''
-      return config
-    }
-  },
-  (error) => Promise.reject(error)
-)
-
 function prepareAuthorizationHeaders(headers: Headers): Headers {
+  headers.set('Access-Control-Allow-Origin', '*')
   const authUser = JSON.parse(localStorage.getItem('authUser') || '')
   if (authUser) {
     headers.set('authorization', `Bearer ${authUser?.accessToken}`)
